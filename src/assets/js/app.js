ab5e83b0e3f598ed7bf585d45cf45f8be9cbd1fd
As <seregadixi@mail.ru>
2022-11-29 18:01:23 +0300

    second commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,24 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    let desctopHelper = document.querySelector('.main-services__desctopHelper')
-    let acc = document.querySelectorAll(".main-services__btn");
-    let i;
-    
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-          this.classList.toggle("activeTab");
-          let panel = this.nextElementSibling;
-          desctopHelper.innerHTML = panel.innerHTML
-          if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-          } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-          }
-        });
-    }
+  let desctopHelper = document.querySelector('.main-services__desctopHelper')
+  let acc = document.querySelectorAll(".main-services__btn");
+  let i;
 
-    acc[0].click()
-    
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("activeTab");
+      let panel = this.nextElementSibling;
+      desctopHelper.innerHTML = panel.innerHTML
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+
+  acc[0].click()
+
+
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiperButtonNext',
+      prevEl: '.swiperButtonPrev',
+    },
+    loop: true,
+    loopedSlides: 4
+  });
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    touchRatio: 0.2,
+    slideToClickedSlide: true,
+    loop: true,
+    loopedSlides: 4
+  });
+  galleryTop.controller.control = galleryThumbs;
+  galleryThumbs.controller.control = galleryTop;
+
+
 })
 
 
